eeaf9d781064c8fabaf0f428cc2681ad31fe1fed
finite-codez <finite.plays.inifinite@gmail.com>
2025-05-21 23:14:53 +0530

    Re-order SettingsProtoDumpUtil.java

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProtoDumpUtil.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProtoDumpUtil.java
@@ -1705,13 +1705,12 @@ class SettingsProtoDumpUtil {
 
         s.dumpHistoricalOperations(p, SecureSettingsProto.HISTORICAL_OPERATIONS);
 
-        dumpSetting(s, p,
-            Settings.Secure.LOCK_SCREEN_WALLPAPER_BLUR,
-            SecureSettingsProto.LOCK_SCREEN_WALLPAPER_BLUR);
-
         // This uses the same order as in SecureSettingsProto.
 
         final long accessibilityToken = p.start(SecureSettingsProto.ACCESSIBILITY);
+        dumpSetting(s, p,
+            Settings.Secure.LOCK_SCREEN_WALLPAPER_BLUR,
+            SecureSettingsProto.LOCK_SCREEN_WALLPAPER_BLUR);
         dumpSetting(s, p,
                 Settings.Secure.ACCESSIBILITY_ENABLED,
                 SecureSettingsProto.Accessibility.ENABLED);
